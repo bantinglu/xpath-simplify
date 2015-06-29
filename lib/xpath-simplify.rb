5d1ae49fbe3aff78dbc6b56787b52d32b713ed20
Scott Mackie <scott@streetcontxt.com>
2015-06-29 11:36:04 -0400

    Basic Features Added

--- lib/xpath-simplify.rb
+++ lib/xpath-simplify.rb
@@ -1,49 +1,73 @@
 class XPathSimplify
-  def self.hi
-    return 'Hi'
-  end
-
   def self.simplify (str)
     arr = str.split(/ /)
+    @f_and = false
+    @f_or = false
+    xp = convert(arr, false, false)
+    return xp
+  end
+
+  def self.convert (arr, f_attach, f_text)
     xp = Array.new
-    xp = convert(arr)
+    i = 0
+    f_attach = "//*" unless f_attach
+    begin
+    while i < arr.length do
+      case arr[i]
+      when '(('    then i += 1; xp[i] = convert(arr[i..arr.length], false, f_text); xp[i] = append_previous(xp, i); i = bracket_increment(arr,i)
+      when '))'    then return xp.join('')
+      when '->'    then i += 1; xp[i] = "[#{arr[i]}]"
+      when '>>'    then i += 1; f_attach = xp[i-1]
+      when '&&'    then if f_text then @f_and = true; return xp.join(''); else i += 1; xp[i] = " and #{convert(arr[i..arr.length], false, false)}"; i+=1; end
+      when '||'    then if f_text then @f_or = true; return xp.join(''); else i += 1; xp[i] = " or #{convert(arr[i..arr.length], false, false)}"; i+=1; end
+      when '::'    then if f_text then return xp.join(''); else i+=2; xp[i] = "#{f_attach}[contains(text(),'#{arr[i-1]}#{convert(arr[i..arr.length], f_attach, true)}')]"; i = text_increment(arr,i); end
+      else              if f_text then xp[i] = " #{arr[i]}"
+        else
+          case arr[i]
+          when /^#.+/                                                           then xp[i] = "#{f_attach}[@id='#{arr[i][1..-1]}']";
+          when /^\..+/                                                          then xp[i] = "#{f_attach}[contains(@class,'#{arr[i][1..-1]}')]";
+          when 'li', 'ul', 'a', 'span', 'button', 'input', 'label', 'textarea'  then xp[i] = "//#{arr[i]}"
+          when /^\/.+/, /^http.*/, /^mailto.*/                                  then xp[i] = "#{f_attach}[contains(@href,'#{arr[i]}')]";
+          else i+=1;                                                                 xp[i] = "#{f_attach}[contains(text(),'#{arr[i-1]}#{convert(arr[i..arr.length], f_attach, true)}')]"; i = text_increment(arr,i); end
+        end
+      end
+      if    @f_and then @f_and = false
+      elsif @f_or  then @f_or = false
+      else  i += 1 end
+    end
+    rescue then return xp.join('')
+    end
     return xp.join('')
   end
 
-  def self.convert (arr)
-    xp = Array.new
-    f_text = false
-    f_attach = "//*"
-    f_and = false
-    f_or = false
-    i = 0
-    begin
-    while i <= arr.length do
-      case arr[i]
-      when '(('    then xp[i] = convert(arr[i..arr.length])
-      when '))'    then return xp
-      when '->'    then i += 1; xp[i] = "[#{arr[i]}]"
-      when '>>'    then i += 1; f_attach = xp[i-1]
-      when '&&'    then i += 1; f_and = xp[i]
-      when '||'    then i += 1; f_or = xp[i]
-      when '::'    then
-        if f_text  then f_text = false; return xp
-        else            f_text = true; xp[i] = convert(arr[i..arr.length]);
-        end
-      when '.'     then xp[i] = "#{f_attach}[contains(@class,'#{arr[i]}']"; f_attach = "//*"
-      when '#'     then xp[i] = "#{f_attach}[@id='#{arr[i]}']"; f_attach = "//*"
-      when 'tag'   then xp[i] = "#{arr[i]}"
-      when 'link'  then xp[i] = "#{f_attach}[contains(@href,'#{arr[i]}']"; f_attach = "//*"
-      else
-        if f_text  then xp[i] = "//#{arr[i]}"
-        else            i += 1; f_text = true; xp[i] = "#{f_attach}[contains(text(),'#{convert(arr[i..arr.length])}')]"; f_attach = "//*"
-        end
-      end
-      i += 1
+  def self.text_increment(arr, i)
+    while i < arr.length do
+      if arr[i] === '::' || arr[i] === '&&' || arr[i] === '||' || arr[i] === '((' || arr[i] === '))' || arr[i] === '->' || arr[i] === '>>'  then return i
+      else i+=1 end
     end
-    rescue
-      return xp
+    return arr.length
+  end
+
+  def self.bracket_increment(arr, i)
+    counter = 0
+    while i < arr.length do
+      if arr[i] === '((' then counter += 1
+      elsif arr[i] === '))' then if counter == 0 then return i; else counter -=1; end
+      else i+=1; end
     end
-    return xp
+    return arr.length
+  end
+
+  def self.append_previous(xp, i)
+    if i>1
+      begin
+      xp[i] = xp[i].gsub(' and //'," and #{xp[i-2]}//")
+      xp[i] = xp[i].gsub(' or //'," or #{xp[i-2]}//")
+      rescue
+        # Do Nothing
+     end
+    end
+    return xp[i]
   end
 end
+
--- test/test_xpath-simplify.rb
+++ test/test_xpath-simplify.rb
@@ -2,15 +2,16 @@ require 'minitest/autorun'
 require 'xpath-simplify'
 
 class XPathTest < Minitest::Test
+
   def test_id
     assert_equal "//*[@id='idname']",
-                 XPathSimplify.simplify('#')
+                 XPathSimplify.simplify('#idname')
   end
-=begin
   def test_class
     assert_equal "//*[contains(@class,'classname')]",
                  XPathSimplify.simplify('.classname')
   end
+
 
   def test_tag
     assert_equal "//li",
@@ -28,16 +29,20 @@ class XPathTest < Minitest::Test
   end
 
   def test_link
-    assert_equal "//*[contains(@class,'classname')]",
-                 XPathSimplify.simplify('htt')
-    assert_equal "//*[contains(@class,'classname')]",
-                 XPathSimplify.simplify('/')
-    assert_equal "//*[contains(@class,'classname')]",
-                 XPathSimplify.simplify('mailto')
+    assert_equal "//*[contains(@href,'http://www.google.com')]",
+                 XPathSimplify.simplify('http://www.google.com')
+    assert_equal "//*[contains(@href,'https://www.google.com')]",
+                 XPathSimplify.simplify('https://www.google.com')
+    assert_equal "//*[contains(@href,'/selection')]",
+                 XPathSimplify.simplify('/selection')
+    assert_equal "//*[contains(@href,'/selection/selection2')]",
+                 XPathSimplify.simplify('/selection/selection2')
+    assert_equal "//*[contains(@href,'mailto:bob@joe.com')]",
+                 XPathSimplify.simplify('mailto:bob@joe.com')
   end
 
   def test_attach
-    assert_equal "//li[4]",
+    assert_equal "//li[3]",
                  XPathSimplify.simplify('li -> 3')
     assert_equal "//ul[6]",
                  XPathSimplify.simplify('ul -> 6')
@@ -45,59 +50,80 @@ class XPathTest < Minitest::Test
                  XPathSimplify.simplify('.classname -> 12')
   end
 
+
   def test_text_plain
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(text(),'This is a text test')]",
                  XPathSimplify.simplify('This is a text test')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(text(),'The button test is here')]",
                  XPathSimplify.simplify('The button test is here')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(text(),'Punctuation? Hopefully.')]",
                  XPathSimplify.simplify('Punctuation? Hopefully.')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(text(),'A span and a input')]",
                  XPathSimplify.simplify('A span and a input')
   end
 
-  def test_text_full
-    assert_equal "//*[@id='remember-me']",
+  def test_text_full_front
+    assert_equal "//*[contains(text(),'This is a text test')]",
                  XPathSimplify.simplify(':: This is a text test')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(text(),'.classname #idname')]",
                  XPathSimplify.simplify(':: .classname #idname')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(text(),'and or and or')]",
                  XPathSimplify.simplify(':: and or and or')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(text(),'a span and a input')]",
                  XPathSimplify.simplify(':: a span and a input')
   end
 
-  def test_and
-    assert_equal "//*[@id='remember-me']",
-                 XPathSimplify.simplify('.classname1 && .classname2')
-    assert_equal "//*[@id='remember-me']",
-                 XPathSimplify.simplify('#idname1 && .classname1')
-    assert_equal "//*[@id='remember-me']",
-                 XPathSimplify.simplify('Text Here && .classname1')
-    assert_equal "//*[@id='remember-me']",
-                 XPathSimplify.simplify('.classname1 && Text Here')
+  def test_text_full_middle
+    assert_equal "//*[contains(text(),'This is a text test')]//*[contains(@class,'classname')]",
+                 XPathSimplify.simplify(':: This is a text test :: .classname')
+    assert_equal "//*[contains(@class,'classname')]//*[contains(text(),'This is a text test')]",
+                 XPathSimplify.simplify('.classname :: This is a text test')
   end
 
+  def test_text_full_middle_nested
+    assert_equal "//*[@id='idname1']//*[contains(text(),'This is a text test')]//*[contains(@class,'classname')]",
+                 XPathSimplify.simplify('#idname1 :: This is a text test :: .classname')
+    assert_equal "//*[contains(text(),'Some Text Here')]//*[contains(text(),'This is a text test')]//*[contains(text(),'And the end')]",
+                 XPathSimplify.simplify('Some Text Here :: This is a text test :: And the end')
+  end
+
+
+  def test_and
+    assert_equal "//*[contains(@class,'classname1')] and //*[contains(@class,'classname2')]",
+                 XPathSimplify.simplify('.classname1 && .classname2')
+    assert_equal "//*[@id='idname1'] and //*[contains(@class,'classname1')]",
+                 XPathSimplify.simplify('#idname1 && .classname1')
+    assert_equal "//*[contains(text(),'Text Here')] and //*[contains(@class,'classname1')]",
+                 XPathSimplify.simplify('Text Here && .classname1')
+    assert_equal "//*[contains(@class,'classname1')] and //*[contains(text(),'Text Here')]",
+                 XPathSimplify.simplify('.classname1 && Text Here')
+    end
+
   def test_or
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(@class,'classname1')] or //*[contains(@class,'classname2')]",
                  XPathSimplify.simplify('.classname1 || .classname2')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[@id='idname1'] or //*[contains(@class,'classname1')]",
                  XPathSimplify.simplify('#idname1 || .classname1')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(text(),'Text Here')] or //*[contains(@class,'classname1')]",
                  XPathSimplify.simplify('Text Here || .classname1')
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(@class,'classname1')] or //*[contains(text(),'Text Here')]",
                  XPathSimplify.simplify('.classname1 || Text Here')
   end
 
   def test_brackets
-    assert_equal "//*[@id='remember-me']",
+    assert_equal "//*[contains(@class,'classname')]",
                  XPathSimplify.simplify('(( .classname ))')
-    assert_equal "//*[@id='remember-me']",
-                 XPathSimplify.simplify('(( Text and other things ))')
-    assert_equal "//*[@id='remember-me']",
-                 XPathSimplify.simplify('(( .classname1 .classname2 ))')
-    assert_equal "//*[@id='remember-me']",
-                 XPathSimplify.simplify('(( .classname1 Text is here ))')
+    assert_equal "//*[contains(@class,'classname')]//*[@id='idname']",
+                 XPathSimplify.simplify('(( .classname #idname ))')
+    assert_equal "//*[contains(@class,'classname')]//*[@id='idname']",
+                 XPathSimplify.simplify('(( .classname )) (( #idname ))')
+    assert_equal "//*[contains(@class,'classname')] and //*[@id='idname']",
+                 XPathSimplify.simplify('(( .classname && #idname ))')
+    assert_equal "//*[contains(@class,'classname')] and //*[@id='idname']",
+                 XPathSimplify.simplify('(( .classname )) && (( #idname ))')
+    assert_equal "//*[contains(@class,'classname')]//*[@id='idname1'] and //*[contains(@class,'classname')]//*[@id='idname2']",
+                 XPathSimplify.simplify('.classname (( #idname1 && #idname2 ))')
+    assert_equal "//*[contains(@class,'classname')]//*[@id='idname1'] or //*[contains(@class,'classname')]//*[@id='idname2']",
+                 XPathSimplify.simplify('.classname (( #idname1 || #idname2 ))')
   end
-=end
 end
